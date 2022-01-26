2023d15cf36e5569ca29e2044dcc32ecdbc968b6
MaaikeWachters <73661817+MaaikeWachters@users.noreply.github.com>
2022-01-26 11:54:44 +0100

    Added Phonetics component

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  
+  return (
+    <div className="Phonetic">
+      
+      <audio controls src={props.phonetic.audio}></audio>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
-
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+
+     <div>
+              <Phonetic phonetic={props.results.phonetics[0]} />
+          
+      </div>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
-              <div key={index}>
-                  <Meaning meaning={meaning} />
-                  </div>
-          )
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
         })}
       </div>
     );
